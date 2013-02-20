70d95cb2e9b36f48ca8a6b24cb4675841c00e8b6
zj <3642214@163.com>
2013-02-20 14:19:58 +0800

    1

--- initShPath.sh
+++ initShPath.sh
@@ -1,3 +1,3 @@
 . Config
 echo " $0 -- not found shell path,create it!"
-mkdir -p /root/git && cd /root/git && git clone $GitServer:/root/git/shell || { echo " $0 -- create shell path error";exit 138;}
+mkdir -p /root/git && cd /root/git && git clone $GitServer:/root/git_work/shell || { echo " $0 -- create shell path error";exit 138;}
