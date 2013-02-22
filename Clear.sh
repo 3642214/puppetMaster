8c1929d93c8418dd6de2e3d17c1ed69cd4682140
zj <3642214@163.com>
2013-02-22 11:08:23 +0800

    1.Clear.sh新增CN、SN、CLT_Master、CLT_Snode参数

--- Clear.sh
+++ Clear.sh
@@ -5,8 +5,9 @@
 		cd /root/git/shell/basic
 		bash git_pull.sh shell || exit $?
 			case "$1" in
-				soft)bash clear_soft.sh|| exit $?;;
+				soft)bash clear_soft.sh || exit $?;;
 				disk)bash clear_disk.sh || exit $?;;
-				alldisk)bash clear_disk.sh all || exit $?;;
+				alldisk)bash clear_disk.sh all || exit $?;;	
+				SN|CN|CLT_Master|CLT_Snode)bash clear_soft.sh $1 || exit $?;;			
 				*)echo " $0 -- clear paremeter error";exit 137;;
 			esac
