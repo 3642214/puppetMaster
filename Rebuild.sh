368fa04988eec87b8ad5b511e67aaa00c43f96e3
zj <3642214@163.com>
2013-03-08 10:58:27 +0800

    修改说明文件

--- Rebuild.sh
+++ Rebuild.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-[ -n "$1" ] || { echo " $0 -- update parameter error:no parameter";exit 135; }
+[ -n "$1" ] || { echo " $0 -- rebuild parameter error:no parameter";exit 135; }
 bash Clear.sh $1 || exit $?
 bash Create.sh || exit $?
