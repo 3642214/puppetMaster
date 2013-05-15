3c9c6e1611b4663e6d2e43ecba98ef54481353c9
zj <3642214@163.com>
2013-05-15 13:31:59 +0800

    修改bug

--- Restart.sh
+++ Restart.sh
@@ -4,11 +4,11 @@
 		cd /root/git/shell/basic
 		bash git_pull.sh shell || exit $?
 		job=`bash readState.sh soft` || { echo " $0 -- readState error";exit 132; }
-				if [ -z "$1" -o $1 = $job ]
+				if [ -z "$1" -o "$1" = $job ]
 					then
 							echo " $0 -- Restart Soft ........."
 							bash restartSoft.sh $job || exit $?
-				elif [ $1 = "CN" -o $1 = "SN" -o $1 = "CLT_Master" -o $1 = "CLT_Snode" ]
+				elif [ "$1" = "CN" -o "$1" = "SN" -o "$1" = "CLT_Master" -o "$1" = "CLT_Snode" ]
 					then
 						exit 0				
 				else
